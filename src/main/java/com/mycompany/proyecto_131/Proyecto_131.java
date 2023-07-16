3d5802a0d9bce19002e0485ec3bf0b1a53951b9c
juan carlos <juan carlos@192.168.0.160>
2023-07-15 23:46:04 -0400

    Iniciando proy

---
+++ src/main/java/com/mycompany/proyecto_131/Proyecto_131.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_131;
+
+/**
+ *
+ * @author juan carlos
+ */
+public class Proyecto_131 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
